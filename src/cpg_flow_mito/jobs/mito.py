de729e7f3df4126d85f32b2b98029b8c6c9e904e
MattWellie <mattwellie@gmail.com>
2025-08-29 15:20:30 +1000

    ARGH

--- src/cpg_flow_mito/jobs/mito.py
+++ src/cpg_flow_mito/jobs/mito.py
@@ -335,8 +335,8 @@ def mito_mutect2(
 
     j = batch_instance.new_bash_job('mito_mutect2', job_attrs | {'tool': 'Mutect2'})
     j.image(config.config_retrieve(['images', 'gatk']))
-    res = resources.STANDARD.set_resources(j, ncpu=4)
-    res.set_to_job(j=j)
+    res = resources.STANDARD.set_resources(j=j, ncpu=4)
+    res.set_to_job(j)
 
     j.declare_resource_group(
         output_vcf={
