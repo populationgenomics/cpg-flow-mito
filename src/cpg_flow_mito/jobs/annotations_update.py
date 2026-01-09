7665ac0734063f041c2e5471d869425ae0230ed5
MattWellie <mattwellie@gmail.com>
2026-01-09 11:23:05 +1000

    include auto-retry behaviour during update

--- src/cpg_flow_mito/jobs/annotations_update.py
+++ src/cpg_flow_mito/jobs/annotations_update.py
@@ -9,6 +9,14 @@ def download_latest_annotations(output_path: Path, job_attrs: dict[str, str]):
     job.image(config.config_retrieve(['images', 'mitoreport']))
 
     # noted that this succeeds locally, but may be fragile. Perhaps a retry wrap makes sense.
-    job.command(f'java -jar mitoreport.jar mito-map-download --output {job.output}')
+    job.command(f"""
+    n=0
+    until [ "$n" -ge 5 ]
+    do
+       java -jar mitoreport.jar mito-map-download --output {job.output} && break
+       n=$((n+1)) 
+       sleep 20
+    done
+    """)
     batch_instance.write_output(job.output, output_path)
     return job
