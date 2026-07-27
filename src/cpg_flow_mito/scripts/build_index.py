ffdc3b62ea9da0c48dbfca095f4b80ee0b0f3d25
EddieLF <eddieformaini@gmail.com>
2026-07-27 12:16:29 +1000

    Filter to active SGs only and use analysis.outputs

--- src/cpg_flow_mito/scripts/build_index.py
+++ src/cpg_flow_mito/scripts/build_index.py
@@ -11,6 +11,7 @@ from pathlib import Path
 
 import jinja2
 from cpg_utils import config, to_path
+from loguru import logger
 from metamist import graphql
 
 JINJA_TEMPLATE_DIR = Path(__file__).absolute().parent.parent / 'templates'
@@ -19,21 +20,18 @@ REPORT_QUERY = graphql.gql(
     """
     query ReportQuery($project: String!, $metaFilter: JSON) {
       project(name: $project) {
-        analyses(type: {eq: "web"}, meta: $metaFilter) {
-          id
-          meta
-          output
-          sequencingGroups {
+        sequencingGroups {
             id
-          }
-          timestampCompleted
+            analyses(type: {eq: "web"}, meta: $metaFilter) {
+              id
+              meta
+              outputs
+              timestampCompleted
+            }
         }
       }
     }"""
 )
-
-WEB_BASE: str = 'gs://cpg-{}-main-web'
-WEB_URL_BASE: str = 'https://main-web.populationgenomics.org.au/{}'
 
 
 def query_for_reports(dataset: str) -> dict[str, dict[str, str]]:
@@ -61,29 +59,23 @@ def query_for_reports(dataset: str) -> dict[str, dict[str, str]]:
         },
     )
 
-    # populate the expected URL portion, and adjust if test
-    web_base = WEB_BASE.format(dataset)
-    if access_level == 'test':
-        web_base = web_base.replace('main', 'test')
+    # iterate over the SGs and grab all with reports
+    for sg in results['project']['sequencingGroups']:
+        if len(sg['analyses']) > 1:
+            logger.warning(f'Multiple MitoReport analyses found for {sg["id"]}, last one will be used in index.')
+        for report in sg['analyses']:
+            meta_dict = report['meta']
+            outpath = to_path(report['outputs']['path']).blob # mito/mitoreport-CPGXXXX/index.html
 
-    # populate the proxy-enabled URL portion, and adjust if test
-    proxy_base = WEB_URL_BASE.format(dataset)
-    if access_level == 'test':
-        proxy_base = proxy_base.replace('main', 'test')
+            web_bucket = config.config_retrieve(['storage', dataset, 'web_url'])
+            url = f'{web_bucket}/{outpath}'
 
-    # iterate over the reports and grab them all
-    for report in results['project']['analyses']:
-        meta_dict = report['meta']
-
-        # pull out the URL from the analysis entry, swap the real URL for a proxy URL
-        url = report['output'].replace(web_base.format(dataset), proxy_base.format(dataset))
-
-        # capture a minrep of all the fields we want to present in the report
-        report_lookup[report['sequencingGroups'][0]['id']] = {
-            'participant': meta_dict['participant_id'],
-            'timestamp': report['timestampCompleted'].split('T')[0],
-            'url': url,
-        }
+            # capture a minrep of all the fields we want to present in the report
+            report_lookup[sg['id']] = {
+                'participant': meta_dict['participant_id'],
+                'timestamp': report['timestampCompleted'].split('T')[0],
+                'url': url,
+            }
 
     return report_lookup
 
